05c191db6c78c29596a18076046a620076dfeec3
Nader <nader_abdelghani@hotmail.com>
2021-06-05 20:46:20 +0200

    Add node types in header file

--- kabsa.h
+++ kabsa.h
@@ -1,9 +1,33 @@
-typedef enum { INTEGER_TYPE, FLOAT_TYPE } numberEnum;
+#include <string>
 
-typedef struct Number {
+typedef enum { INTEGER_TYPE, FLOAT_TYPE } numberEnum;
+typedef enum { CONSTANT_TYPE, VARIABLE_TYPE, FUNCTION_TYPE } identifierEnum;
+typedef enum { NUMBER_TYPE, IDENTIFIER_TYPE, OPERATION_TYPE } nodeEnum;
+
+typedef struct {
     numberEnum type;
     union {
         int integer_value;
         float float_value;
     };
-} Number;
+} NumberNode;
+
+typedef struct {
+    identifierEnum type;
+    std::string key;
+} IdentifierNode;
+
+typedef struct {
+    int operator_token;
+    int num_of_operands;
+    struct NodeType *operands[1];
+} OperationNode;
+
+typedef struct Node {
+    nodeEnum type;
+    union {
+        NumberNode number;
+        IdentifierNode identifier;
+        OperationNode operation;
+    };
+} Node;
